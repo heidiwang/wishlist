d7cdcbb8dc4938c1aa5525ab2eb0b5a63a77ed70
andylamb <andrew.lamb@stanford.edu>
2014-02-06 00:12:12 -0800

    Modified so it connects to db (on my computer)

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ http.createServer(app).listen(app.get("port"), function(){
 });
 
 function init_mongoose () {
-	mongoose.connect("mongodb://localhost/test");
+	mongoose.connect("mongodb://10.31.12.51/test");
 	var db = mongoose.connection;
 	db.on("error", console.error.bind(console, "connection error:"));
 	db.once("open", function callback () {
